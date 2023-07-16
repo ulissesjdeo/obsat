b7bc53d356977ad4b0f181ef42aca48e1f09752f
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2023-07-16 18:16:05 -0300

    Developing battery.py code.

--- modules/battery.py
+++ modules/battery.py
@@ -1,4 +1,9 @@
-# TODO
+from machine import Pin, ADC
+from time import sleep
+
 
 def level():
-    return
+    potentiometer = ADC(28)
+    potentiometer_value = potentiometer.read_u16()
+    print(potentiometer_value)
+    sleep(0.25)
