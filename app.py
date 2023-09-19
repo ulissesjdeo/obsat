ccd512a76c5e3ca6f5af0675ad77312cce710454
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2023-09-19 15:17:29 -0300

    Fixing code.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from calendar import timegm
 from os.path import exists
 from time import gmtime
+from os import makedirs
 
 
 if not exists('debug.lock'):
@@ -10,21 +11,31 @@ else:
     DEBUG = True
     from modules import json, image
 
-if __name__ == '__main__':
-    timestamp = timegm(gmtime())
-    if not DEBUG:
-        battery = battery.level()
-        temperature = temperature.data()
-        position = position.data()
-        file = camera.capture(timestamp)
-    else:
-        battery = 95
-        temperature = 25
-        position = {"g": [-0.298, -0.298, -0.298], "a": [-0.298, -0.298, -0.298]}
-        file = 'other/debug.jpg'
+try:
+    makedirs('data')
+except FileExistsError:
+    pass
 
-    people = image.analyze(file, timestamp)
+try:
+    makedirs('photos')
+except FileExistsError:
+    pass
 
-    with open(f'data/{timestamp}.json', 'w') as data:
-        data.write(json.finalize(battery, temperature, position, people))
-        data.close()
+timestamp = timegm(gmtime())
+
+if not DEBUG:
+    battery = battery.level()
+    temperature = temperature.data()
+    position = position.data()
+    file = camera.capture(timestamp)
+else:
+    battery = 95
+    temperature = 25
+    position = {"g": [-0.298, -0.298, -0.298], "a": [-0.298, -0.298, -0.298]}
+    file = 'other/debug.jpg'
+
+people = image.analyze(file, timestamp)
+
+with open(f'data/{timestamp}.json', 'w') as data:
+    data.write(json.finalize(battery, temperature, position, people))
+    data.close()
