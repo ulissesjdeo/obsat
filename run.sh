5865eb4b122b651c3dab7c2ba788b538ed2c1a0e
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2023-09-27 21:25:50 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cd /home/obsat/obsat || exit
+cd /home/obsat/obsat 2> /dev/null
 python app.py
 filename=data/$(find data/ -type f -exec ls -t1 {} + | head -1) && filename=$(echo "$filename" | sed -e 's/data\///')
 python request.py "$filename"
